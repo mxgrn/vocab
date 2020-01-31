91abbc0bf59c41fb011511e2778a85f8e13d12f1
Max Gorin <m.gorin@bluecode.com>
2020-01-31 08:04:58 +0700

    Don't need many DB connections

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ config :vocab, Vocab.Repo,
   database: "vocab_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 1
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
