70e63357502094257ca9a9afd72ce5e62b344456
Max Gorin <regzok@gmail.com>
2024-11-30 18:45:01 +0000

    Minor config updates

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ config :vocab, Vocab.Repo,
   database: "vocab_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 1
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
